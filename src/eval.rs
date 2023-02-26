b32c4677035704619c9c2065477c115c9eed84d9
Alex Gartrell <alexgartrell@gmail.com>
2023-02-26 09:47:32 -0800

    Add evaluation logic
    This is useful for debugging

---
+++ src/eval.rs
@@ -0,0 +1,62 @@
+use crate::ir::{self, IR};
+use std::collections::HashMap;
+
+#[derive(Default)]
+struct State {
+    mem: HashMap<i32, i8>,
+    idx: i32,
+}
+
+impl State {
+    fn read(&self, off: i32) -> i8 {
+        *self.mem.get(&(self.idx + off)).unwrap_or(&0)
+    }
+
+    fn write(&mut self, off: i32, val: i8) {
+        self.mem.insert(self.idx + off, val);
+    }
+
+    fn ptr_change(&mut self, amt: i32) {
+        self.idx += amt;
+    }
+}
+
+pub fn eval(prog: &ir::IRProgram) {
+    let mut state = State::default();
+    fn run_series(irs: &[ir::IR], state: &mut State) {
+        for ir in irs {
+            match ir {
+                IR::Loop(inner) => {
+                    while state.read(0) != 0 {
+                        run_series(inner, state);
+                    }
+                }
+                IR::PtrChange(amt) => {
+                    state.ptr_change(*amt);
+                }
+                IR::Add(amt) => {
+                    state.write(0, state.read(0) + amt);
+                }
+                IR::Putch => unsafe {
+                    libc::putchar(state.read(0) as libc::c_int);
+                },
+                IR::Getch => unsafe {
+                    state.write(0, libc::getchar() as i8);
+                },
+                IR::SimpleLoop(delta, inner) => {
+                    while state.read(0) != 0 {
+                        run_series(inner, state);
+                        state.write(0, state.read(0) + delta);
+                    }
+                }
+                IR::AddMul(off, amt) => {
+                    state.write(*off, state.read(*off) + state.read(0) * amt);
+                }
+                IR::MovImm(val) => {
+                    state.write(0, *val);
+                }
+            }
+        }
+    }
+    run_series(&prog.0, &mut state);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod ast;
 mod c_emitter;
+mod eval;
 mod ir;
 mod optimize;
 mod parser;
@@ -8,6 +9,7 @@ mod x86_emitter;
 
 use clap::{Parser, ValueEnum};
 use std::io::Read;
+use std::process::ExitCode;
 
 #[derive(Debug, Clone, ValueEnum)]
 enum Arch {
@@ -23,25 +25,46 @@ struct Args {
     nostdlib: bool,
     #[arg(long)]
     explore: bool,
+    #[arg(long)]
+    eval: bool,
     #[arg(long, value_enum, default_value = "x86_64")]
     arch: Arch,
+
+    path: Option<std::path::PathBuf>,
 }
 
-fn main() {
+fn main() -> ExitCode {
     let args = Args::parse();
-    let mut code = String::new();
-    std::io::stdin().read_to_string(&mut code).unwrap();
+
+    if args.eval && args.path.is_none() {
+        eprintln!("Error: cannot eval when reading program from stdin");
+        return ExitCode::from(2);
+    }
+
+    let code = if let Some(ref path) = args.path {
+        std::fs::read_to_string(path).unwrap()
+    } else {
+        let mut code = String::new();
+        std::io::stdin().read_to_string(&mut code).unwrap();
+        code
+    };
 
     let ast_prog = parser::Parser::parse(&code);
     let ir_prog = ir::IRProgram::from_ast_program(&ast_prog);
+    if args.eval {
+        eval::eval(&ir_prog);
+        return ExitCode::SUCCESS;
+    }
+
     let ir_prog = optimize::optimize(&ir_prog);
     if args.explore {
         println!("{:#?}", ir_prog);
-        return;
+        return ExitCode::SUCCESS;
     }
     match args.arch {
         Arch::X86_64 => x86_emitter::X86Emitter::emit(&ir_prog, args.nostdlib),
         Arch::RiscV => riscv_emitter::RiscVEmitter::emit(&ir_prog, args.nostdlib),
         Arch::C => c_emitter::CEmitter::emit(&ir_prog, args.nostdlib),
     }
+    return ExitCode::SUCCESS;
 }
